a6d75a1c7ee6bb67a3b3770d86851e295f50fff4
horie1024 <ryosuke.pic@gmail.com>
2015-03-20 01:01:03 +0900

    テストコードとcircle.ymlを追加

---
+++ app/src/test/java/jp/horie1024/gradlejvmtestsample/MainActivityTest.java
@@ -0,0 +1,17 @@
+package jp.horie1024.gradlejvmtestsample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void テスト(){
+
+    }
+}
