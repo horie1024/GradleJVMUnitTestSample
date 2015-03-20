915fb8daf7eb71cc6b845ccbd1df4bbb1d05a483
horie1024 <ryosuke.pic@gmail.com>
2015-03-20 13:37:03 +0900

    テストコード変更

--- app/src/test/java/jp/horie1024/gradlejvmtestsample/MainActivityTest.java
+++ app/src/test/java/jp/horie1024/gradlejvmtestsample/MainActivityTest.java
@@ -1,17 +1,36 @@
 package jp.horie1024.gradlejvmtestsample;
 
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MainActivityTest {
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    MainActivity mActivity;
 
     @Before
     public void setUp() throws Exception {
+        super.setUp();
 
+        setActivityInitialTouchMode(false);
+
+        mActivity = getActivity();
     }
 
     @Test
-    public void テスト(){
+    public void testApp() {
 
+        assertTrue(mActivity != null);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
     }
 }
